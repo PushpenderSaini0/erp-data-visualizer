d802667e3b0efc0acfcb192981043c5b2e0455a4
PushpenderSaini0 <54404738+PushpenderSaini0@users.noreply.github.com>
2021-06-13 17:59:48 +0530

    Added dataProcessor module

---
+++ js/dataProcessor.js
@@ -0,0 +1,61 @@
+//get all unique cource from the list
+export function getCourses(data) {
+    const cources = new Set();
+    data.forEach(item => cources.add(item.courseName));
+    return [...cources];
+}
+
+//get attandance for one cource
+export function getAttandance(data, cource) {
+    const dates = [];
+    const classAttended = [];
+    let classAttendedCounter = 0;
+    const courseName = cource;
+    let totalClasses = 0;
+    let present = 0;
+    let absent = 0;
+    let leaveOfAbsence = 0;
+    data.forEach(item => {
+        if (cource.localeCompare(item.courseName) == 0) {
+            totalClasses++;
+            dates.push(item.date);
+            if ("Present".localeCompare(item.status) == 0) {
+                present++;
+                classAttended.push(++classAttendedCounter);
+            }
+            if ("Absent".localeCompare(item.status) == 0) {
+                absent++;
+                classAttended.push(classAttendedCounter);
+            }
+            if ("Leave of Absence".localeCompare(item.status) == 0) {
+                leaveOfAbsence++;
+                classAttended.push(classAttendedCounter);
+            }
+        }
+    });
+    return { courseName, totalClasses, present, absent, leaveOfAbsence, dates, classAttended };
+}
+
+export function getAbsentSummary(data, cource) {
+    const absentSummary = [];
+    data.forEach(item => {
+        if (cource.localeCompare(item.courseName) == 0) {
+            if ("Absent".localeCompare(item.status) == 0) {
+                absentSummary.push({ date: item.date, time: item.time })
+            }
+        }
+    });
+    return absentSummary;
+}
+
+export function getLeaveOfAbsenceSummary(data, cource) {
+    const absentSummary = [];
+    data.forEach(item => {
+        if (cource.localeCompare(item.courseName) == 0) {
+            if ("Leave of Absence".localeCompare(item.status) == 0) {
+                absentSummary.push({ date: item.date, time: item.time })
+            }
+        }
+    });
+    return absentSummary;
+}
