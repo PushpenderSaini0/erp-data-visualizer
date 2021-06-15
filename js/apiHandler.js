9a1c1768b8011b601c8e19e312238276ea702a77
PushpenderSaini0 <54404738+PushpenderSaini0@users.noreply.github.com>
2021-06-16 01:27:20 +0530

    Updated API endpoint to Cloud Function

--- js/apiHandler.js
+++ js/apiHandler.js
@@ -1,12 +1,12 @@
 export const getERPData = (mode) => {
     if (mode === "SID") {
         const sid = document.getElementById("sid").value;
-        const url = 'https://notes.pushpendersaini.com/tools/erpdvapi/getAbsentSummaryData.php?sid=';
+        const url = 'https://asia-south1-edvapi-1a78a.cloudfunctions.net/getAbsentSummary?sid=';
         return fetch(url + sid).then(r => r.json());
     }
     const uname = document.getElementById("uname").value;
     const passwd = document.getElementById("passwd").value;
     const auth = btoa(btoa(uname) + ":" + (btoa(passwd)));
-    const url = 'https://notes.pushpendersaini.com/tools/erpdvapi/getAbsentSummaryData.php?auth=';
+    const url = 'https://asia-south1-edvapi-1a78a.cloudfunctions.net/getAbsentSummary?auth=';
     return fetch(url + auth).then(r => r.json());
 }
