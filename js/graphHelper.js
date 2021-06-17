e0ab660222f9bbfca4b7fc2d407b1a333e607a92
Praanjjal <58231223+recursive-PJ@users.noreply.github.com>
2021-06-17 20:21:06 +0530

    Visual change to main chart

--- js/graphHelper.js
+++ js/graphHelper.js
@@ -25,25 +25,41 @@ export function plotGraph(data) {
         datasets: [{
             type: 'line',
             label: '# Present',
-            borderColor: 'rgb(0,0,255)',
-            borderWidth: 2,
             fill: false,
+            // borderJoinStyle: 'round',
+            borderWidth: 2,
+            borderColor: "#118AB2",
+            pointBorderColor: "#118AB2",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointRadius: 4,
+            pointHitRadius: 4,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 2,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(0,153,153,1)",
             data: plotYP
         }, {
             type: 'line',
             label: '# 70% Mark',
-            backgroundColor: 'rgb(255,0,0)',
             data: plotYS,
             fill: false,
-            borderColor: 'rgb(255,0,0)',
+            borderColor: '#F71735',
+            borderDash: [5,5],
+            pointRadius: 0,
             borderWidth: 2
         }, {
             type: 'bar',
             label: '# Total Classes',
             backgroundColor: 'rgb(0,255,0)',
             data: plotY,
-            borderColor: 'white',
-            borderWidth: 2
+
+
+         
+            backgroundColor: '#9EE493',
+            borderColor: "#6CD65C",
+            borderWidth: 2,
+        
         }]
 
     };
